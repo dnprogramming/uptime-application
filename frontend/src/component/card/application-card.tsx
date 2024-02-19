b35c9c1cbc1890be4f536c38fdec48391050b243
dnprogramming <speedcomputersdn@gmail.com>
2024-02-18 22:55:09 -0500

    Adding Card

--- frontend/src/component/card/application-card.tsx
+++ frontend/src/component/card/application-card.tsx
@@ -5,12 +5,26 @@ import {ApplicationInformation} from '../../generated/report_pb';
 
 const AppCard = styled.div`
   display: flex;
-  background: #ff0000;
+`;
+
+const AppName = styled.div`
+  width: 100dvw;
+`;
+
+const Updated = styled.div`
+  width: 100dvw;
 `;
 
 function ApplicationCard({app}: {app: ApplicationInformation}) {
   const appName = app.getAppname();
-  return <AppCard>{appName}</AppCard>;
+  const lastUpdated = app.getLastupdated();
+  const updated = lastUpdated?.toDate().toLocaleString();
+  return (
+    <AppCard>
+      <AppName>{appName}</AppName>
+      <Updated>{updated}</Updated>
+    </AppCard>
+  );
 }
 
 export default ApplicationCard;
