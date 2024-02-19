b561ceeb90056e5b30590f81c6fd60571fd3007b
dnprogramming <speedcomputersdn@gmail.com>
2024-02-18 23:47:20 -0500

    Working on Application Card

--- frontend/src/component/card/application-card.tsx
+++ frontend/src/component/card/application-card.tsx
@@ -5,24 +5,48 @@ import {ApplicationInformation} from '../../generated/report_pb';
 
 const AppCard = styled.div`
   display: flex;
+  width: 100dvw;
 `;
-
+const StatusDiv = styled.div`
+  width: 25dvw;
+`;
+const StatusCircle = styled.div<{status: Number}>`
+  width: 80dvw;
+  height: 80dvh;
+  border-radius: 25%;
+  background-color: ${props =>
+    props.status === 0 ? 'Green' : props.status === 1 ? 'Yellow' : 'Red'};
+`;
+const AppDiv = styled.div`
+  width: 75dvw;
+`;
 const AppName = styled.div`
   width: 100dvw;
 `;
-
+const PersonResponsible = styled.div`
+  width: 100dvw;
+`;
 const Updated = styled.div`
   width: 100dvw;
 `;
 
 function ApplicationCard({app}: {app: ApplicationInformation}) {
   const appName = app.getAppname();
+  const appStatus = app.getAppstatus();
   const lastUpdated = app.getLastupdated();
-  const updated = lastUpdated?.toDate().toLocaleString();
+  const updated = lastUpdated?.toDate().toLocaleString().replace(',', ' at');
+  const responsible = app.getResponsiblepersonname();
+  const appId = app.getAppid();
   return (
-    <AppCard>
-      <AppName>{appName}</AppName>
-      <Updated>{updated}</Updated>
+    <AppCard key={appId}>
+      <StatusDiv>
+        <StatusCircle status={appStatus} />
+      </StatusDiv>
+      <AppDiv>
+        <AppName>{appName}</AppName>
+        <PersonResponsible>{responsible}</PersonResponsible>
+        <Updated>Last Updated: {updated}</Updated>
+      </AppDiv>
     </AppCard>
   );
 }
